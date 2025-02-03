88d27fcd283dd48822d297a0bc4b36ae2912b634
Darcy Best <darcy.best.89@gmail.com>
2025-02-03 13:32:05 -0700

    Get clang working
    Also reverts the IsOkAndHolds change done previously. It is now exactly what was in the original repo (modulo a namespace change). Apparently we were just using the wrong namespace (::testing:: instead of ::moriarty::)

--- src/variables/constraints/container_constraints_test.cc
+++ src/variables/constraints/container_constraints_test.cc
@@ -27,6 +27,7 @@
 namespace moriarty {
 namespace {
 
+using ::moriarty::IsOkAndHolds;
 using ::moriarty_testing::Context;
 using ::moriarty_testing::GeneratedValuesAre;
 using ::moriarty_testing::GenerateLots;
@@ -38,33 +39,26 @@ using ::testing::Le;
 
 TEST(ContainerConstraintsTest, LengthConstraintsAreCorrect) {
   EXPECT_THAT(Length(10).GetConstraints(), GeneratedValuesAre(10));
-
-  // TODO: This are really "IsOkAndHolds" tests, but the matcher is not working
-  auto val1 = GenerateLots(Length("2 * N").GetConstraints(),
-                           Context().WithValue<MInteger>("N", 7));
-  MORIARTY_EXPECT_OK(val1);
-  EXPECT_THAT(*val1, Each(14));
-
-  auto val2 = GenerateLots(Length(AtLeast("X"), AtMost(15)).GetConstraints(),
-                           Context().WithValue<MInteger>("X", 3));
-  MORIARTY_EXPECT_OK(val2);
-  EXPECT_THAT(*val2, Each(AllOf(Ge(3), Le(15))));
+  EXPECT_THAT(GenerateLots(Length("2 * N").GetConstraints(),
+                           Context().WithValue<MInteger>("N", 7)),
+              IsOkAndHolds(Each(14)));
+  EXPECT_THAT(GenerateLots(Length(AtLeast("X"), AtMost(15)).GetConstraints(),
+                           Context().WithValue<MInteger>("X", 3)),
+              IsOkAndHolds(Each(AllOf(Ge(3), Le(15)))));
 }
 
 TEST(ContainerConstraintsTest, ElementsConstraintsAreCorrect) {
   EXPECT_THAT(Elements<MInteger>(Between(1, 10)).GetConstraints(),
               GeneratedValuesAre(AllOf(Ge(1), Le(10))));
-  auto val = GenerateLots(
-      Elements<MInteger>(AtLeast("X"), AtMost(15)).GetConstraints(),
-      Context().WithValue<MInteger>("X", 3));
-  MORIARTY_EXPECT_OK(val);
-  EXPECT_THAT(*val, Each(AllOf(Ge(3), Le(15))));
+  EXPECT_THAT(GenerateLots(
+                  Elements<MInteger>(AtLeast("X"), AtMost(15)).GetConstraints(),
+                  Context().WithValue<MInteger>("X", 3)),
+              IsOkAndHolds(Each(AllOf(Ge(3), Le(15)))));
 }
 
 TEST(ContainerConstraintsTest, LengthToStringWorks) {
   EXPECT_THAT(Length(Between(1, 10)).ToString(),
               AllOf(HasSubstr("Length"), HasSubstr("1, 10")));
 }
-
 }  // namespace
 }  // namespace moriarty
