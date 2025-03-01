47eb6386edd0ab63cbb62bc70ff97f5e0baff9cd
Darcy Best <darcy.best.89@gmail.com>
2025-02-28 21:28:50 -0700

    Add BasicRandomContext
    Most of this is copied from RandomConfig. We will be able to delete that soon, once Contexts are done.

---
+++ src/contexts/internal/basic_random_context.h
@@ -0,0 +1,321 @@
+/*
+ * Copyright 2025 Darcy Best
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MORIARTY_SRC_CONTEXTS_INTERNAL_BASIC_RANDOM_CONTEXT_H_
+#define MORIARTY_SRC_CONTEXTS_INTERNAL_BASIC_RANDOM_CONTEXT_H_
+
+#include <cstdint>
+#include <functional>
+#include <iterator>
+#include <span>
+#include <stdexcept>
+#include <unordered_set>
+
+#include "src/internal/random_engine.h"
+
+namespace moriarty {
+namespace moriarty_internal {
+
+template <typename T>
+using ElementType = std::iter_value_t<decltype(std::begin(T{}))>;
+
+// BasicRandomContext
+//
+// A class to handle Moriarty-agnostic randomness (RandomInteger,
+// DistinctIntegers, RandomPermutation, etc).
+class BasicRandomContext {
+ public:
+  explicit BasicRandomContext(RandomEngine& engine) : engine_(engine) {};
+
+  // RandomInteger()
+  //
+  // Returns a random integer in the closed interval [min, max].
+  int64_t RandomInteger(int64_t min, int64_t max);
+
+  // RandomInteger()
+  //
+  // Returns a random integer in the semi-closed interval [0, n). Useful for
+  // random indices.
+  int64_t RandomInteger(int64_t n);
+
+  // Shuffle()
+  //
+  // Shuffles the elements in `container`.
+  template <typename Container>
+  void Shuffle(Container& container);
+
+  // RandomElement()
+  //
+  // Returns a random element of `container`.
+  template <typename Container>
+  ElementType<Container> RandomElement(const Container& container);
+
+  // RandomElementsWithReplacement()
+  //
+  // Returns k (randomly ordered) elements of `container`, possibly with
+  // duplicates.
+  template <typename Container>
+  std::vector<ElementType<Container>> RandomElementsWithReplacement(
+      const Container& container, int k);
+
+  // RandomElementsWithoutReplacement()
+  //
+  // Returns k (randomly ordered) elements of `container`, without duplicates.
+  //
+  // Each element may appear at most once. Note that if there are duplicates in
+  // `container`, each of those could be returned once each.
+  //
+  // So RandomElementsWithoutReplacement({0, 1, 1, 1}, 2) could return {1, 1}.
+  template <typename Container>
+  std::vector<ElementType<Container>> RandomElementsWithoutReplacement(
+      const Container& container, int k);
+
+  // RandomPermutation()
+  //
+  // Returns a random permutation of {0, 1, ... , n-1}.
+  std::vector<int> RandomPermutation(int n);
+
+  // RandomPermutation()
+  //
+  // Returns a random permutation of {min, min + 1, ... , min + (n-1)}.
+  //
+  // Requires min + (n-1) to not overflow T.
+  template <typename T>
+    requires std::integral<T>
+  std::vector<T> RandomPermutation(int n, T min);
+
+  // DistinctIntegers()
+  //
+  // Returns k (randomly ordered) distinct integers from
+  // {min, min + 1, ... , min + (n-1)}.
+  //
+  // Requires min + (n-1) to not overflow T.
+  template <typename T>
+    requires std::integral<T>
+  std::vector<T> DistinctIntegers(T n, int k, T min = 0);
+
+  // RandomComposition()
+  //
+  // Returns a random composition. A composition partitions n objects
+  // into k "buckets". This function returns a vector of size k representing the
+  // number of objects in each bucket.
+  //
+  // Example: (11, 22) means the first bucket has 11 elements and the second
+  // bucket has 22 elements. (22, 11) is a different composition.
+  //
+  // A lower bound on bucket size can be set with `min_bucket_size`. It is most
+  // common that this is 0 or 1.
+  //
+  // Requires n + (k - 1) to not overflow T.
+  template <typename T>
+    requires std::integral<T>
+  std::vector<T> RandomComposition(T n, int k, T min_bucket_size = 1);
+
+ private:
+  std::reference_wrapper<RandomEngine> engine_;
+
+  // Shuffles so that the first k elements are the prefix of a random shuffle.
+  // The last n-k elements are not shuffled, and likely in pseudo-increasing
+  // order.
+  template <typename Container>
+  void PartialShuffle(Container& container, int64_t k);
+};
+
+// -----------------------------------------------------------------------------
+//  Template implementation below
+
+template <typename Container>
+void BasicRandomContext::PartialShuffle(Container& container, int64_t k) {
+  std::span c(container);
+  if (!(0 <= k && k <= c.size())) {
+    throw std::runtime_error(
+        std::format("PartialShuffle(<container>, {}) is invalid, with "
+                    "<container>.size()={} (need 0 <= k <= size)",
+                    k, container.size()));
+  }
+  for (int i = 0; i < k; i++) {
+    int64_t s = RandomInteger(i, c.size() - 1);  // size()-1 is safe
+    if (i != s) std::swap(c[i], c[s]);
+  }
+}
+
+template <typename Container>
+void BasicRandomContext::Shuffle(Container& container) {
+  PartialShuffle(container, container.size());
+}
+
+template <typename Container>
+ElementType<Container> BasicRandomContext::RandomElement(
+    const Container& container) {
+  std::span c(container);
+  if (c.empty()) {
+    throw std::runtime_error("RandomElement() called with empty c.");
+  }
+
+  int64_t idx = RandomInteger(c.size());
+  return c[idx];
+}
+
+template <typename Container>
+std::vector<ElementType<Container>>
+BasicRandomContext::RandomElementsWithReplacement(const Container& container,
+                                                  int k) {
+  std::span c(container);
+  using T = ElementType<Container>;
+
+  if (k < 0) {
+    throw std::runtime_error(
+        std::format("RandomElementsWithReplacement(<container>, {}) is invalid "
+                    "(need k >= 0)",
+                    k));
+  }
+  if (c.empty() && k > 0) {
+    throw std::runtime_error(
+        std::format("RandomElementsWithReplacement(<empty_container>, {}) is "
+                    "invalid (need nonempty container)",
+                    k));
+  }
+
+  std::vector<T> result;
+  result.reserve(k);
+  for (int i = 0; i < k; i++) result.push_back(c[RandomInteger(c.size())]);
+
+  return result;
+}
+
+template <typename Container>
+std::vector<ElementType<Container>>
+BasicRandomContext::RandomElementsWithoutReplacement(const Container& container,
+                                                     int k) {
+  std::span c(container);
+  using T = ElementType<Container>;
+
+  if (k < 0) {
+    throw std::runtime_error(
+        std::format("RandomElementsWithoutReplacement(<container>, {}) is "
+                    "invalid (need k >= 0)",
+                    k));
+  }
+  if (k > c.size()) {
+    throw std::runtime_error(
+        std::format("RandomElementsWithReplacement(<container>, {}) is invalid "
+                    "since <container>.size() == {} (need k <= size)",
+                    k, c.size()));
+  }
+
+  std::vector<T> result;
+  result.reserve(k);
+
+  auto indices = DistinctIntegers(c.size(), k);
+  for (size_t index : indices) result.push_back(c[index]);
+  return result;
+}
+
+template <typename T>
+  requires std::integral<T>
+std::vector<T> BasicRandomContext::RandomPermutation(int n, T min) {
+  if (n < 0) {
+    throw std::runtime_error(std::format(
+        "RandomPermutation({}, {}) is invalid (need n >= 0)", n, min));
+  }
+  return DistinctIntegers(n, n, min);
+}
+
+template <typename T>
+  requires std::integral<T>
+std::vector<T> BasicRandomContext::DistinctIntegers(T n, int k, T min) {
+  if (!(0 <= k && k <= n)) {
+    throw std::runtime_error(std::format(
+        "DistinctIntegers({}, {}, {}) is invalid (need 0 <= k <= n)", n, k,
+        min));
+  }
+
+  // If we are asking for a large percentage of the range, we just generate all
+  // values and shuffle that list so we don't have to deal with the (minor)
+  // overhead of hash sets, plus the potential pathological cases of the
+  // sampling failing many times. 4 is mostly arbitrary.
+  if (4 * k >= n) {
+    std::vector<T> all;
+    all.reserve(n);
+    for (int i = 0; i < n; i++) all.push_back(i + min);
+    PartialShuffle(all, k);
+    all.resize(k);
+    return all;
+  }
+
+  std::unordered_set<T> sample;
+  sample.reserve(k);
+
+  std::vector<T> result;
+  result.reserve(k);
+
+  while (result.size() < k) {
+    T value = min + RandomInteger(n);
+    auto [it, inserted] = sample.insert(value);
+    if (inserted) result.push_back(value);
+  }
+
+  return result;
+}
+
+template <typename T>
+  requires std::integral<T>
+std::vector<T> BasicRandomContext::RandomComposition(T n, int k,
+                                                     T min_bucket_size) {
+  if (n == 0 && k == 0 && min_bucket_size == 0) return std::vector<T>();
+
+  if (n < 0 || k <= 0 || min_bucket_size < 0) {
+    throw std::runtime_error(std::format(
+        "RandomComposition({}, {}, {}) is invalid (need n >= 0, k > 0, "
+        "min_bucket_size >= 0)",
+        n, k, min_bucket_size));
+  }
+
+  if (min_bucket_size > 0 && n / min_bucket_size < k) {
+    throw std::runtime_error(
+        std::format("RandomComposition({}, {}, {}) is impossible: "
+                    "min_bucket_size uses more than n elements already",
+                    n, k, min_bucket_size));
+  }
+
+  if (min_bucket_size > 0) {
+    auto result = RandomComposition(n - min_bucket_size * k, k, 0);
+    for (T& val : result) val += min_bucket_size;
+    return result;
+  }
+  if (n == 0) return std::vector<T>(k, 0);
+
+  auto barriers = DistinctIntegers(n + (k - 1), k - 1);
+  std::sort(barriers.begin(), barriers.end());
+
+  std::vector<T> result;
+  result.reserve(k);
+
+  T prev = -1;
+  for (T b : barriers) {
+    result.push_back(b - prev - 1);
+    prev = b;
+  }
+  result.push_back((n + (k - 1)) - prev - 1);
+
+  return result;
+}
+
+}  // namespace moriarty_internal
+}  // namespace moriarty
+
+#endif  // MORIARTY_SRC_CONTEXTS_INTERNAL_BASIC_RANDOM_CONTEXT_H_
---
+++ src/contexts/internal/basic_random_context_test.cc
@@ -0,0 +1,495 @@
+/*
+ * Copyright 2025 Darcy Best
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "src/contexts/internal/basic_random_context.h"
+
+#include <map>
+#include <set>
+#include <type_traits>
+#include <vector>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "src/internal/random_engine.h"
+
+namespace moriarty {
+namespace moriarty_internal {
+namespace {
+
+using ::testing::AllOf;
+using ::testing::AnyOfArray;
+using ::testing::Each;
+using ::testing::ElementsAre;
+using ::testing::Ge;
+using ::testing::IsEmpty;
+using ::testing::Le;
+using ::testing::Lt;
+using ::testing::SizeIs;
+using ::testing::UnorderedElementsAre;
+using ::testing::WhenSorted;
+
+// This is a *massively* simplified check to see if a distribution is
+// approximately uniform.
+MATCHER_P(IsApproximatelyUniformlyRandom, expected_number_of_buckets, "") {
+  // This check is extremely naive. If the expected number of items in a bucket
+  // is X, then this accepts as long as the number in each bucket is in the
+  // range [X/3, 5*X/3]. (Those bounds were picked somewhat arbitrarily). This
+  // moreso tests that each value "appears" than it checks for uniformity. This
+  // can be improved to use Chi-Squared for a better test if needed.
+  int expected_items_per_bucket = arg.size() / expected_number_of_buckets;
+  int expected_lower = expected_items_per_bucket / 3;
+  int expected_upper = 5 * expected_items_per_bucket / 3;
+
+  if (expected_lower <= 5) {
+    *result_listener
+        << "This test is not effective if each bucket contains <= 5 items.";
+    return false;
+  }
+
+  // arg_type                           == std::vector<Foo>
+  // std::decay_t<arg_type>::value_type == Foo
+  using sample_type = typename std::decay_t<arg_type>::value_type;
+  std::map<sample_type, int> count;
+  for (const auto& item : arg) count[item]++;
+
+  if (count.size() != expected_number_of_buckets) {
+    *result_listener << "actual number of buckets (" << count.size()
+                     << ") is not expected (" << expected_number_of_buckets
+                     << ")";
+    return false;
+  }
+
+  for (const auto& [bucket, num_occurrence] : count) {
+    if (num_occurrence < expected_lower || num_occurrence > expected_upper) {
+      *result_listener << testing::PrintToString(bucket) << " occurs "
+                       << num_occurrence
+                       << " times, which is outside the acceptable ["
+                       << expected_lower << ", " << expected_upper << "] range";
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// -----------------------------------------------------------------------------
+//  RandomInteger()
+
+TEST(BasicRandomContextTest, RandomIntegerInTypicalCase) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_EQ(ctx.RandomInteger(314, 314), 314);
+  EXPECT_EQ(ctx.RandomInteger(1), 0);
+  EXPECT_THAT(ctx.RandomInteger(1, 5), AllOf(Ge(1), Le(5)));
+  EXPECT_THAT(ctx.RandomInteger(-10, -5), AllOf(Ge(-10), Le(-5)));
+  EXPECT_THAT(ctx.RandomInteger(4), AllOf(Ge(0), Lt(4)));
+}
+
+TEST(BasicRandomContextTest, RandomIntegerForInvalidRangesShouldFail) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THROW({ ctx.RandomInteger(5, 3); }, std::runtime_error);
+  EXPECT_THROW({ ctx.RandomInteger(0); }, std::runtime_error);
+  EXPECT_THROW({ ctx.RandomInteger(-5); }, std::runtime_error);
+}
+
+TEST(BasicRandomContextTest,
+     RandomIntegerGivesApproximatelyTheRightDistribution) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  {  // Double argument:
+    std::vector<int> samples;
+    for (int i = 0; i < 6000; i++) samples.push_back(ctx.RandomInteger(23, 34));
+    EXPECT_THAT(samples,
+                IsApproximatelyUniformlyRandom(12));  // 12=34-23+1
+  }
+
+  {  // Single argument:
+    std::vector<int> samples;
+    for (int i = 0; i < 6000; i++) samples.push_back(ctx.RandomInteger(13));
+    EXPECT_THAT(samples, IsApproximatelyUniformlyRandom(13));
+  }
+}
+
+// -----------------------------------------------------------------------------
+//  Shuffle()
+
+TEST(BasicRandomContextTest, ShufflingAnEmptyArrayIsSuccessful) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<int> empty;
+  ctx.Shuffle(empty);
+  EXPECT_THAT(empty, IsEmpty());
+}
+
+TEST(BasicRandomContextTest, ShufflePermutesElements) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  // Shuffle integers
+  std::vector<int> int_vals = {1, 22, 333, 4444, 55555};
+  ctx.Shuffle(int_vals);
+  EXPECT_THAT(int_vals, UnorderedElementsAre(1, 22, 333, 4444, 55555));
+
+  // Shuffle strings
+  std::vector<std::string> str_vals = {"these", "are", "some", "words"};
+  ctx.Shuffle(str_vals);
+  EXPECT_THAT(str_vals, UnorderedElementsAre("these", "are", "some", "words"));
+}
+
+TEST(BasicRandomContextTest, ShuffleGivesApproximatelyTheRightDistribution) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<std::vector<int>> samples;
+  std::vector<int> v = {1, 2, 3, 4};
+  for (int i = 0; i < 6000; i++) {
+    ctx.Shuffle(v);
+    samples.push_back(v);
+  }
+  EXPECT_THAT(samples, IsApproximatelyUniformlyRandom(24));  // 24 = 4!
+}
+
+// -----------------------------------------------------------------------------
+//  RandomElement()
+
+TEST(BasicRandomContextTest,
+     RandomElementGivesApproximatelyTheRightDistribution) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<int> samples;
+  std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  for (int i = 0; i < 6000; i++) samples.push_back(ctx.RandomElement(v));
+
+  EXPECT_THAT(samples, IsApproximatelyUniformlyRandom(10));
+}
+
+TEST(BasicRandomContextTest, RandomElementGivesSomeValueFromArray) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<int> v = {123456, 456789, 789123};
+  EXPECT_THAT(ctx.RandomElement(v), AnyOfArray(v));
+}
+
+TEST(BasicRandomContextTest, RandomElementFromAnEmptyContainerShouldFail) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<int> empty;
+  EXPECT_THROW({ ctx.RandomElement(empty); }, std::runtime_error);
+}
+
+// -----------------------------------------------------------------------------
+//  RandomElements()
+
+TEST(BasicRandomContextTest,
+     RandomElementsWithReplacementGivesApproximatelyTheRightDistribution) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<std::vector<int>> samples;
+  std::vector<int> v = {1, 2, 3, 4, 5};
+  for (int i = 0; i < 6000; i++)
+    samples.push_back(ctx.RandomElementsWithReplacement(v, 3));
+
+  EXPECT_THAT(samples, IsApproximatelyUniformlyRandom(125));  // 125 = 5^3
+}
+
+TEST(BasicRandomContextTest,
+     RandomElementsWithoutReplacementGivesApproximatelyTheRightDistribution) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<std::vector<int>> samples;
+  std::vector<int> v = {1, 2, 3, 4, 5};
+  for (int i = 0; i < 6000; i++)
+    samples.push_back(ctx.RandomElementsWithoutReplacement(v, 3));
+
+  // 60 = (5 choose 3) * 3!
+  EXPECT_THAT(samples, IsApproximatelyUniformlyRandom(60));
+}
+
+TEST(BasicRandomContextTest, RandomElementsGivesSomeValuesFromArray) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<std::string> v = {"123456", "456789", "789123"};
+  EXPECT_THAT(ctx.RandomElementsWithReplacement(v, 5), Each(AnyOfArray(v)));
+  EXPECT_THAT(ctx.RandomElementsWithoutReplacement(v, 2), Each(AnyOfArray(v)));
+}
+
+TEST(BasicRandomContextTest, RandomElementsObeysDistinctOnlyPolicy) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<int> v = {11, 22, 33, 44};
+
+  // Trial 20 times. Each trial has 4/64 chance of false positive.
+  for (int i = 0; i < 20; i++) {
+    EXPECT_THAT(ctx.RandomElementsWithoutReplacement(v, 3),
+                WhenSorted(AnyOfArray<std::vector<int>>(
+                    {{11, 22, 33}, {11, 33, 44}, {11, 22, 44}, {22, 33, 44}})));
+  }
+}
+
+TEST(BasicRandomContextTest,
+     RandomElementsWithReplacementCanContainDuplicates) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<int> v = {11, 22, 33, 44};
+
+  // Only 4 elements and I'm querying for 6. Duplicates must be there!
+  // Not crashing is the test.
+  (void)ctx.RandomElementsWithReplacement(v, 6);
+}
+
+TEST(BasicRandomContextTest, RandomElementsWithNegativeInputShouldFail) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<int> v = {11, 22, 33, 44};
+
+  EXPECT_THROW(
+      { ctx.RandomElementsWithReplacement(v, -1); }, std::runtime_error);
+  EXPECT_THROW(
+      { ctx.RandomElementsWithoutReplacement(v, -1); }, std::runtime_error);
+}
+
+TEST(BasicRandomContextTest, RandomElementsFromAnEmptyContainerShouldFail) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<std::string> empty;
+  EXPECT_THROW(
+      { ctx.RandomElementsWithReplacement(empty, 1); }, std::runtime_error);
+  EXPECT_THROW(
+      { ctx.RandomElementsWithoutReplacement(empty, 1); }, std::runtime_error);
+}
+
+TEST(BasicRandomContextTest,
+     RandomElementsFromAnEmptyContainerShouldReturnNothing) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<std::string> empty;
+  EXPECT_THAT(ctx.RandomElementsWithReplacement(empty, 0), IsEmpty());
+  EXPECT_THAT(ctx.RandomElementsWithoutReplacement(empty, 0), IsEmpty());
+}
+
+TEST(BasicRandomContextTest, RandomElementsAskingForTooManyDistinctShouldFail) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<std::string> v = {"123456", "456789", "789123"};
+  EXPECT_THROW(
+      { ctx.RandomElementsWithoutReplacement(v, 4); }, std::runtime_error);
+}
+
+// -----------------------------------------------------------------------------
+//  RandomPermutation()
+
+TEST(BasicRandomContextTest, RandomPermutationOfNegativeSizeShouldFail) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THROW({ ctx.RandomPermutation(-1); }, std::runtime_error);
+}
+
+TEST(BasicRandomContextTest, RandomPermutationOfSizeZeroIsOk) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THAT(ctx.RandomPermutation(0), IsEmpty());
+}
+
+TEST(BasicRandomContextTest, RandomPermutationGeneratesAPermutation) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THAT(ctx.RandomPermutation(5), UnorderedElementsAre(0, 1, 2, 3, 4));
+  EXPECT_THAT(ctx.RandomPermutation(5, 10),
+              UnorderedElementsAre(10, 11, 12, 13, 14));
+}
+
+TEST(BasicRandomContextTest, RandomPermutationGivesPermutation) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THAT(ctx.RandomPermutation(0), IsEmpty());
+  EXPECT_THAT(ctx.RandomPermutation(1), UnorderedElementsAre(0));
+  EXPECT_THAT(ctx.RandomPermutation(4), UnorderedElementsAre(0, 1, 2, 3));
+  EXPECT_THAT(ctx.RandomPermutation(6), UnorderedElementsAre(0, 1, 2, 3, 4, 5));
+}
+
+TEST(BasicRandomContextTest,
+     RandomPermutationGivesApproximatelyCorrectDistribution) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<std::vector<int>> samples;
+  for (int i = 0; i < 6000; i++) samples.push_back(ctx.RandomPermutation(3));
+
+  EXPECT_THAT(samples, IsApproximatelyUniformlyRandom(6));  // 6 = 3!
+}
+
+// -----------------------------------------------------------------------------
+//  DistinctIntegers()
+
+TEST(BasicRandomContextTest,
+     DistinctIntegersGivesApproximatelyTheRightDistribution) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<std::vector<int>> samples;
+  for (int i = 0; i < 6000; i++) samples.push_back(ctx.DistinctIntegers(8, 2));
+
+  EXPECT_THAT(samples, IsApproximatelyUniformlyRandom(56));  // 56 = 8 * 7
+}
+
+TEST(BasicRandomContextTest, DistinctIntegersWithNegativeNOrKShouldFail) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THROW({ ctx.DistinctIntegers(-1, 5); }, std::runtime_error);
+  EXPECT_THROW({ ctx.DistinctIntegers(10, -5); }, std::runtime_error);
+}
+
+TEST(BasicRandomContextTest, DistinctIntegersWithKGreaterThanNShouldFail) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THROW({ ctx.DistinctIntegers(5, 10); }, std::runtime_error);
+}
+
+TEST(BasicRandomContextTest, DistinctIntegersWithZeroIsFine) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THAT(ctx.DistinctIntegers(0, 0), IsEmpty());
+  EXPECT_THAT(ctx.DistinctIntegers(5, 0), IsEmpty());
+}
+
+TEST(BasicRandomContextTest, DistinctIntegersDoesNotGiveDuplicates) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THAT(ctx.DistinctIntegers(10, 10),
+              UnorderedElementsAre(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+  std::vector<int> vals = ctx.DistinctIntegers(50, 30);
+  EXPECT_THAT(std::set<int>(vals.begin(), vals.end()), SizeIs(30));
+}
+
+TEST(BasicRandomContextTest, DistinctIntegersOffsetWorksAppropriately) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THAT(ctx.DistinctIntegers(10000, 100, 1234),
+              Each(AllOf(Ge(1234), Le(10000 + 1234))));
+}
+
+// -----------------------------------------------------------------------------
+//  RandomComposition()
+
+TEST(BasicRandomContextTest,
+     RandomCompositionGivesApproximatelyCorrectDistribution) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  std::vector<std::vector<int>> samples;
+  for (int i = 0; i < 6000; i++) samples.push_back(ctx.RandomComposition(5, 3));
+
+  // (1, 1, 3), (1, 2, 2), (1, 3, 1), (2, 1, 2), (2, 2, 1), (3, 1, 1)
+  EXPECT_THAT(samples, IsApproximatelyUniformlyRandom(6));
+}
+
+TEST(BasicRandomContextTest, RandomCompositionWithNegativeArgumentShouldFail) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THROW({ ctx.RandomComposition(-1, 5); }, std::runtime_error);
+  EXPECT_THROW({ ctx.RandomComposition(10, -5); }, std::runtime_error);
+  EXPECT_THROW({ ctx.RandomComposition(10, 5, -1); }, std::runtime_error);
+}
+
+TEST(BasicRandomContextTest, RandomCompositionWithZeroBucketsAndZeroSumIsFine) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THAT(ctx.RandomComposition(0, 0, 0), IsEmpty());
+}
+
+TEST(BasicRandomContextTest,
+     RandomCompositionWithZeroBucketsAndNonZeroSumFails) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THROW({ ctx.RandomComposition(5, 0); }, std::runtime_error);
+}
+
+TEST(BasicRandomContextTest, RandomCompositionWithTooLargeMinBucketShouldFail) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THROW({ ctx.RandomComposition(4, 2, 3); }, std::runtime_error);
+}
+
+TEST(BasicRandomContextTest, RandomCompositionWithExactMinBucketIsFine) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THAT(ctx.RandomComposition(10, 5, 2), ElementsAre(2, 2, 2, 2, 2));
+}
+
+TEST(BasicRandomContextTest, RandomCompositionRespectsMinBucketSize) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THAT(ctx.RandomComposition(40, 6, 2), Each(Ge(2)));
+}
+
+TEST(BasicRandomContextTest, RandomCompositionReturnsTheRightNumberOfBuckets) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  EXPECT_THAT(ctx.RandomComposition(40, 6, 2), SizeIs(6));
+  EXPECT_THAT(ctx.RandomComposition(400, 3, 0), SizeIs(3));
+}
+
+TEST(BasicRandomContextTest, RandomCompositionHasCorrectTotalSum) {
+  RandomEngine engine({1, 2, 3}, "v0.1");
+  BasicRandomContext ctx(engine);
+
+  auto sum = [](const std::vector<int>& v) {
+    return std::accumulate(v.begin(), v.end(), 0);
+  };
+
+  EXPECT_EQ(sum(ctx.RandomComposition(40, 6, 2)), 40);
+  EXPECT_EQ(sum(ctx.RandomComposition(400, 3, 0)), 400);
+  EXPECT_EQ(sum(ctx.RandomComposition(555, 10)), 555);
+}
+
+}  // namespace
+}  // namespace moriarty_internal
+}  // namespace moriarty
