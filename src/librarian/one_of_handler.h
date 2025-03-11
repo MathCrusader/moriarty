7dc3763ff7f16ffe2b136b7b47f5226ce3362918
Darcy Best <darcy.best.89@gmail.com>
2025-03-11 12:22:52 -0600

    Add OneOfHandler
    No one uses this right now. We have to decide if the MVariable base-class is going to continue holding variables or not. If they are, then this class is probably not needed (it still could be used for separation of logic).
    
    The trade-off is needing to have oneof logic throughout all generic code or oneof logic throughout each MVariable's code. Both seem not great.

---
+++ src/librarian/one_of_handler.h
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2025 Darcy Best
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MORIARTY_SRC_LIBRARIAN_ONEOF_HANDLER_H_
+#define MORIARTY_SRC_LIBRARIAN_ONEOF_HANDLER_H_
+
+#include <algorithm>
+#include <optional>
+#include <stdexcept>
+#include <vector>
+
+namespace moriarty {
+namespace librarian {
+
+// OneOfHandler
+//
+// Maintains a list of possible values for a variable. Initially, all values are
+// considered possible. As new calls come in, values are removed from the list
+// of possibilities.
+template <typename T>
+class OneOfHandler {
+ public:
+  // HasBeenConstrained()
+  //
+  // Returns true if any calls to ConstrainOptions() have been made.
+  [[nodiscard]] bool HasBeenConstrained() const;
+
+  // ConstrainOptions()
+  //
+  // Adds additional constraints that this object must be one of `one_of`. This
+  // will intersect these values with any existing options.
+  //
+  // If there are no valid options remaining after the intersection, this will
+  // throw an exception.
+  template <typename Container>
+  void ConstrainOptions(Container&& one_of);
+
+  // IsSatisfiedWith()
+  //
+  // Determines if the given value is one of the valid values.
+  [[nodiscard]] bool IsSatisfiedWith(const T& value) const;
+
+  // SelectOneOf()
+  //
+  // Returns a random value from the list of possible values. rand_fn should
+  // take exactly one argument, an integer, and return an integer in the range
+  // [0, n). The underlying items may or may not be well-ordered. Do not depend
+  // on any particular ordering.
+  template <typename RandFn>
+  [[nodiscard]] T SelectOneOf(RandFn&& rand_fn) const;
+
+  // GetUniqueValue()
+  //
+  // Returns the unique value if there is only one possible value.
+  std::optional<T> GetUniqueValue() const;
+
+  // GetValidOptions()
+  //
+  // Returns a list of all valid values.
+  //
+  // Precondition: HasBeenConstrained() == true
+  [[nodiscard]] const std::vector<T>& GetValidOptions() const;
+
+ private:
+  std::optional<std::vector<T>> valid_options_;
+};
+
+// -----------------------------------------------------------------------------
+//  Template implementation below
+
+template <typename T>
+bool OneOfHandler<T>::HasBeenConstrained() const {
+  return valid_options_.has_value();
+}
+
+template <typename T>
+bool OneOfHandler<T>::IsSatisfiedWith(const T& value) const {
+  return !valid_options_ ||
+         std::find(valid_options_->begin(), valid_options_->end(), value) !=
+             valid_options_->end();
+}
+
+template <typename T>
+template <typename RandFn>
+T OneOfHandler<T>::SelectOneOf(RandFn&& rand_fn) const {
+  return *std::next(valid_options_->begin(), rand_fn(valid_options_->size()));
+}
+
+template <typename T>
+std::optional<T> OneOfHandler<T>::GetUniqueValue() const {
+  if (!valid_options_ || valid_options_->size() != 1) return std::nullopt;
+  return valid_options_->front();
+}
+
+template <typename T>
+const std::vector<T>& OneOfHandler<T>::GetValidOptions() const {
+  return *valid_options_;
+}
+
+template <typename T>
+template <typename Container>
+void OneOfHandler<T>::ConstrainOptions(Container&& one_of) {
+  if (!valid_options_.has_value()) {
+    valid_options_ = std::vector<T>();
+    std::ranges::copy(one_of, std::back_inserter(*valid_options_));
+  } else {
+    std::erase_if(*valid_options_, [&one_of](const T& value) {
+      return std::ranges::find(one_of, value) == one_of.end();
+    });
+  }
+
+  if (valid_options_->empty()) {
+    throw std::runtime_error(
+        "No valid options remaining after calls to Exactly/OneOf.");
+  }
+}
+
+}  // namespace librarian
+}  // namespace moriarty
+
+#endif  // MORIARTY_SRC_LIBRARIAN_ONEOF_HANDLER_H_
---
+++ src/librarian/one_of_handler_test.cc
@@ -0,0 +1,137 @@
+// Copyright 2025 Darcy Best
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/librarian/one_of_handler.h"
+
+#include <stdexcept>
+#include <vector>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace moriarty {
+namespace librarian {
+namespace {
+
+using ::testing::AnyOf;
+using ::testing::UnorderedElementsAre;
+
+TEST(OneOfHandler, HasBeenConstrainedReturnsFalseInitially) {
+  OneOfHandler<int> handler;
+  EXPECT_FALSE(handler.HasBeenConstrained());
+}
+
+TEST(OneOfHandler, HasBeenConstrainedReturnsTrueAfterAnyCall) {
+  OneOfHandler<int> handler;
+  handler.ConstrainOptions(std::vector{1, 2, 3});
+  EXPECT_TRUE(handler.HasBeenConstrained());
+}
+
+TEST(OneOfHandler, IsSatisfiedWithReturnsIsSatisfiedWithAnythingInitially) {
+  OneOfHandler<int> handler;
+  EXPECT_TRUE(handler.IsSatisfiedWith(1));
+  EXPECT_TRUE(handler.IsSatisfiedWith(-2));
+  EXPECT_TRUE(handler.IsSatisfiedWith(3321578));
+}
+
+TEST(OneOfHandler, IsSatisfiedWithReturnsAsExpectedForConstrainedValue) {
+  OneOfHandler<int> handler;
+  handler.ConstrainOptions(std::vector{1, 2, 3});
+  EXPECT_TRUE(handler.IsSatisfiedWith(1));
+  EXPECT_TRUE(handler.IsSatisfiedWith(2));
+  EXPECT_TRUE(handler.IsSatisfiedWith(3));
+  EXPECT_FALSE(handler.IsSatisfiedWith(4));
+
+  handler.ConstrainOptions(std::vector{1, 2});
+  EXPECT_TRUE(handler.IsSatisfiedWith(1));
+  EXPECT_TRUE(handler.IsSatisfiedWith(2));
+  EXPECT_FALSE(handler.IsSatisfiedWith(3));
+  EXPECT_FALSE(handler.IsSatisfiedWith(4));
+}
+
+TEST(OneOfHandler, SelectOneOfReturnsOneOfTheConstrainedValues) {
+  OneOfHandler<int> handler;
+  handler.ConstrainOptions(std::vector{1, 2, 3});
+  EXPECT_THAT(handler.SelectOneOf([](int n) { return n - 1; }), AnyOf(1, 2, 3));
+}
+
+TEST(OneOfHandler, GetUniqueValueReturnsTheUniqueValueIfThereIsOne) {
+  {  // No constraints
+    OneOfHandler<int> handler;
+    EXPECT_EQ(handler.GetUniqueValue(), std::nullopt);
+  }
+  {  // One value
+    OneOfHandler<int> handler;
+    handler.ConstrainOptions(std::vector{1});
+    EXPECT_EQ(handler.GetUniqueValue(), 1);
+  }
+  {  // One value after intersection
+    OneOfHandler<int> handler;
+    handler.ConstrainOptions(std::vector{1, 3});
+    handler.ConstrainOptions(std::vector{2, 3});
+    EXPECT_EQ(handler.GetUniqueValue(), 3);
+  }
+  {  // Multiple values
+
+    OneOfHandler<int> handler;
+    handler.ConstrainOptions(std::vector{1, 4, 7});
+    EXPECT_EQ(handler.GetUniqueValue(), std::nullopt);
+    handler.ConstrainOptions(std::vector{4, 7, 10});
+    EXPECT_EQ(handler.GetUniqueValue(), std::nullopt);
+  }
+}
+
+TEST(OneOfHandler, GetValidOptionsReturnsAllValidOptions) {
+  {
+    OneOfHandler<int> handler;
+    handler.ConstrainOptions(std::vector{1, 2, 3});
+    EXPECT_THAT(handler.GetValidOptions(), UnorderedElementsAre(1, 2, 3));
+    handler.ConstrainOptions(std::vector{1, 2});
+    EXPECT_THAT(handler.GetValidOptions(), UnorderedElementsAre(1, 2));
+    handler.ConstrainOptions(std::vector{2});
+    EXPECT_THAT(handler.GetValidOptions(), UnorderedElementsAre(2));
+  }
+  {
+    OneOfHandler<int> handler;
+    handler.ConstrainOptions(std::vector{1, 2, 3});
+    handler.ConstrainOptions(std::vector{2, 3, 4});
+    EXPECT_THAT(handler.GetValidOptions(), UnorderedElementsAre(2, 3));
+  }
+}
+
+TEST(OneOfHandler, ConstrainOptionsLeftWithNoValidOptionsShouldThrow) {
+  {  // No options initially
+    OneOfHandler<int> handler;
+    EXPECT_THROW(
+        { handler.ConstrainOptions(std::vector<int>()); }, std::runtime_error);
+  }
+  {  // No options after intersection
+    OneOfHandler<int> handler;
+    handler.ConstrainOptions(std::vector{1, 2, 3});
+    EXPECT_THROW(
+        { handler.ConstrainOptions(std::vector{4, 5, 6}); },
+        std::runtime_error);
+  }
+  {  // No options after multiple intersections
+    OneOfHandler<int> handler;
+    handler.ConstrainOptions(std::vector{1, 2, 3});
+    handler.ConstrainOptions(std::vector{2, 3, 4});
+    EXPECT_THROW(
+        { handler.ConstrainOptions(std::vector{1, 4}); }, std::runtime_error);
+  }
+}
+
+}  // namespace
+}  // namespace librarian
+}  // namespace moriarty
