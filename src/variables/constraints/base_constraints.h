3e1958a2b1d9aec4cc8f3a55c5cc769d7f9ce63c
Darcy Best <darcy.best.89@gmail.com>
2025-03-13 17:59:56 -0600

    Revamp Exactly and OneOf constructors
    This should be more resilient for the short-term. These will be used largely in code, so we want a good user experience. This allows for OneOf({"abc", "def"}), which casts it to an std::string, so it is safe to store without them having to do type conversions or say `OneOf<std::string>({ ... })`.
    
    We should probably create a typedef that resolves Moriarty types (e.g., vector<tuple<int, string>> -> vector<tuple<int64_t, string>>). But this needs more thought before we do it to see where it fits in (if anywhere).

--- src/variables/constraints/base_constraints.h
+++ src/variables/constraints/base_constraints.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2025 Darcy Best
  * Copyright 2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,8 +20,13 @@
 
 #include <concepts>
 #include <cstdint>
+#include <format>
+#include <functional>
+#include <initializer_list>
 #include <string>
 #include <string_view>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
 namespace moriarty {
@@ -29,6 +35,19 @@ namespace moriarty {
 class MConstraint {};
 
 // Constraint stating that the variable must be exactly this value.
+//
+// Examples:
+//  * Exactly(10) accepts only the integer 10.
+//  * Exactly({1, 2, 3}) accepts only the vector {1, 2, 3}.
+//
+// NOTE:
+// We have added several special constructors to handle different types.
+// This should make working with basic types nicer. For non-basic types, you may
+// need to specify the type explicitly:
+//   E.g.,
+//         Exactly<std::tuple<int, int>>({2, 4})) or
+//         Exactly(std::tuple<int, int>({2, 4})) or
+//         std::tuple<int, int> x = {2, 4}; Exactly(x);
 template <typename T>
 class Exactly : public MConstraint {
  public:
@@ -36,61 +55,170 @@ class Exactly : public MConstraint {
   // E.g., `Exactly(10)` or `Exactly(std::vector<int64_t>{1, 2, 3})`
   explicit Exactly(T value);
 
+  // Special constructor for integral types -> int64_t
+  template <std::integral IntLike>
+    requires std::same_as<T, int64_t>
+  explicit Exactly(IntLike num);
+
+  // Special constructor for string-like types (const char*, std::string_view)
+  template <std::convertible_to<std::string_view> StringLike>
+    requires std::same_as<T, std::string>
+  explicit Exactly(StringLike str);
+
+  // Special constructor for std::span<E> -> std::vector<E>
+  template <typename ElemType>
+    requires std::same_as<T, std::vector<std::remove_cv_t<ElemType>>>
+  explicit Exactly(std::span<ElemType> sp);
+
+  // Special constructor for std::initializer_list<E> -> std::vector<E>
+  template <typename ElemType>
+    requires std::same_as<T, std::vector<std::remove_cv_t<ElemType>>>
+  explicit Exactly(std::initializer_list<ElemType> sp);
+
   // Returns the value that the variable must be.
   [[nodiscard]] T GetValue() const;
+
+  // Returns a human-readable representation of this constraint. Uses
+  // `to_string` to print `value` in the message.
+  [[nodiscard]] std::string ToString(
+      std::function<std::string(const T&)> to_string) const;
+
+  // Determines if all constraints are satisfied with the given value.
+  [[nodiscard]] bool IsSatisfiedWith(const T& value) const;
+
+  // Returns a string explaining why the value does not satisfy the constraints.
+  // It is assumed that IsSatisfiedWith() returned false.
+  // Uses `to_string` to print `value` in the message.
+  [[nodiscard]] std::string Explanation(
+      std::function<std::string(const T&)> to_string, const T& value) const;
 
  private:
   T value_;
 };
 
-// Constraint stating that the variable must be exactly this string value.
-// Specialization for string-like types.
-template <>
-class Exactly<std::string> : public MConstraint {
- public:
-  // The string variable must be exactly this value.
-  // E.g., Exactly("abc");
-  explicit Exactly(std::string_view value);
-
-  // Returns the value that the variable must be.
-  [[nodiscard]] std::string GetValue() const;
-
- private:
-  std::string value_;
-};
-
 // The variable must be one of these values.
 // Examples:
 //  * OneOf({1, 2, 4})
-//  * OneOf({"a", "b", "c"})
-//  * OneOf(1, 2, 4)
-//  * OneOf("Possible", "Impossible")
+//  * OneOf({"3 * N + 1", "1", "X"})
+//  * OneOf({"Possible", "Impossible"})
 template <typename T>
 class OneOf : public MConstraint {
  public:
-  template <typename Container>
-  explicit OneOf(Container&& options);
+  // Constructor for std::vector<T>
+  explicit OneOf(std::vector<T> options);
+
+  // Constructor for std::span<T> -> Convert to std::vector<T>
+  explicit OneOf(std::span<typename std::remove_cv<T>::type> options);
+
+  // Constructor for std::initializer_list<T> -> Convert to std::vector<T>
   explicit OneOf(std::initializer_list<T> options);
-  explicit OneOf(T options...);
+
+  // Convert integral types to int64_t
+  template <std::integral IntLike>
+    requires std::same_as<T, int64_t>
+  explicit OneOf(std::vector<IntLike> options);
+
+  template <std::integral IntLike>
+    requires std::same_as<T, int64_t>
+  explicit OneOf(std::span<IntLike> options);
+
+  template <std::integral IntLike>
+    requires std::same_as<T, int64_t>
+  explicit OneOf(std::initializer_list<IntLike> options);
+
+  // Convert string-like types to std::string
+  template <std::convertible_to<std::string_view> StringLike>
+    requires std::same_as<T, std::string>
+  explicit OneOf(std::vector<StringLike> options);
+
+  template <std::convertible_to<std::string_view> StringLike>
+    requires std::same_as<T, std::string>
+  explicit OneOf(std::span<StringLike> options);
+
+  template <std::convertible_to<std::string_view> StringLike>
+    requires std::same_as<T, std::string>
+  explicit OneOf(std::initializer_list<StringLike> options);
 
   // Returns the values that the variable must be one of.
   [[nodiscard]] std::vector<T> GetOptions() const;
+
+  // Returns a human-readable representation of this constraint. Uses
+  // `to_string` to print `options` in the message.
+  [[nodiscard]] std::string ToString(
+      std::function<std::string(const T&)> to_string) const;
+
+  // Determines if all constraints are satisfied with the given value.
+  [[nodiscard]] bool IsSatisfiedWith(const T& value) const;
+
+  // Returns a string explaining why the value does not satisfy the constraints.
+  // It is assumed that IsSatisfiedWith() returned false.
+  // Uses `to_string` to print `options` and `value` in the message.
+  [[nodiscard]] std::string Explanation(
+      std::function<std::string(const T&)> to_string, const T& value) const;
 
  private:
   std::vector<T> options_;
 };
 
-// Remap all integer types (via CTAD) to MInteger-compatible type.
-// E.g., Exactly(10) is remapped to Exactly<int64_t>(10)
-template <typename T>
-  requires std::integral<T>
-Exactly(T) -> Exactly<int64_t>;
+// -----------------------------------------------------------------------------
+//  CTAD for Exactly<>
 
-// Remap all string types (via CTAD) to MString-compatible type.
-// E.g., Exactly("abc") is remapped to Exactly<std::string>("abc")
+// General deduction guide
 template <typename T>
-  requires std::convertible_to<T, std::string_view>
-Exactly(T) -> Exactly<std::string>;
+Exactly(T) -> Exactly<T>;
+
+// Special deduction guide for integral types
+template <std::integral IntLike>
+Exactly(IntLike) -> Exactly<int64_t>;
+
+// Special deduction guide for string-like types
+template <std::convertible_to<std::string_view> StringLike>
+Exactly(StringLike) -> Exactly<std::string>;
+
+// Special deduction guide for std::span<T> -> std::vector<T>
+template <typename ElemType>
+Exactly(std::span<ElemType>)
+    -> Exactly<std::vector<std::remove_cv_t<ElemType>>>;
+
+// Special deduction guide for std::initializer_list<T> -> std::vector<T>
+template <typename ElemType>
+Exactly(std::initializer_list<ElemType>)
+    -> Exactly<std::vector<std::remove_cv_t<ElemType>>>;
+
+// -----------------------------------------------------------------------------
+//  CTAD for OneOf<>
+// * We need all combinations of
+//      (vector, span, initializer_list) and (general, integer, string).
+
+// General deduction guide
+template <typename T>
+OneOf(std::vector<T>) -> OneOf<T>;
+
+template <typename T>
+OneOf(std::span<T>) -> OneOf<T>;
+
+template <typename T>
+OneOf(std::initializer_list<T>) -> OneOf<T>;
+
+// Special deduction guide for integral types
+template <std::integral IntLike>
+OneOf(std::vector<IntLike>) -> OneOf<int64_t>;
+
+template <std::integral IntLike>
+OneOf(std::span<IntLike>) -> OneOf<int64_t>;
+
+template <std::integral IntLike>
+OneOf(std::initializer_list<IntLike>) -> OneOf<int64_t>;
+
+// Special deduction guide for string-like types
+template <std::convertible_to<std::string_view> StringLike>
+OneOf(std::vector<StringLike>) -> OneOf<std::string>;
+
+template <std::convertible_to<std::string_view> StringLike>
+OneOf(std::span<StringLike>) -> OneOf<std::string>;
+
+template <std::convertible_to<std::string_view> StringLike>
+OneOf(std::initializer_list<StringLike>) -> OneOf<std::string>;
 
 // -----------------------------------------------------------------------------
 //  Template Implementation Below
@@ -99,8 +227,171 @@ template <typename T>
 Exactly<T>::Exactly(T value) : value_(std::move(value)) {}
 
 template <typename T>
+template <std::integral IntLike>
+  requires std::same_as<T, int64_t>
+Exactly<T>::Exactly(IntLike num) : value_(static_cast<int64_t>(num)) {
+  if (!std::in_range<int64_t>(num)) {
+    throw std::invalid_argument(
+        std::format("{} does not fit into int64_t in Exactly", num));
+  }
+}
+
+template <typename T>
+template <std::convertible_to<std::string_view> StringLike>
+  requires std::same_as<T, std::string>
+Exactly<T>::Exactly(StringLike str) : value_(std::string(std::move(str))) {}
+
+template <typename T>
+template <typename ElemType>
+  requires std::same_as<T, std::vector<std::remove_cv_t<ElemType>>>
+Exactly<T>::Exactly(std::span<ElemType> sp)
+    : value_(std::vector<ElemType>(sp.begin(), sp.end())) {}
+
+template <typename T>
+template <typename ElemType>
+  requires std::same_as<T, std::vector<std::remove_cv_t<ElemType>>>
+Exactly<T>::Exactly(std::initializer_list<ElemType> sp)
+    : value_(sp.begin(), sp.end()) {}
+
+template <typename T>
 T Exactly<T>::GetValue() const {
   return value_;
+}
+
+template <typename T>
+std::string Exactly<T>::ToString(
+    std::function<std::string(const T&)> to_string) const {
+  return std::format("is exactly {}", to_string(value_));
+}
+
+template <typename T>
+bool Exactly<T>::IsSatisfiedWith(const T& value) const {
+  return value == value_;
+}
+
+template <typename T>
+std::string Exactly<T>::Explanation(
+    std::function<std::string(const T&)> to_string, const T& value) const {
+  return std::format("`{}` is not exactly `{}`", to_string(value),
+                     to_string(value_));
+}
+
+template <typename T>
+OneOf<T>::OneOf(std::vector<T> options) : options_(std::move(options)) {}
+
+template <typename T>
+OneOf<T>::OneOf(std::span<typename std::remove_cv<T>::type> options)
+    : options_(options.begin(), options.end()) {}
+
+template <typename T>
+OneOf<T>::OneOf(std::initializer_list<T> options) : options_(options) {}
+
+template <typename T>
+template <std::integral IntLike>
+  requires std::same_as<T, int64_t>
+OneOf<T>::OneOf(std::vector<IntLike> options) {
+  options_.reserve(options.size());
+  for (IntLike v : options) {
+    if (!std::in_range<int64_t>(v)) {
+      throw std::invalid_argument(
+          std::format("{} does not fit into int64_t in OneOf", v));
+    }
+    options_.push_back(static_cast<int64_t>(v));
+  }
+}
+
+template <typename T>
+template <std::integral IntLike>
+  requires std::same_as<T, int64_t>
+OneOf<T>::OneOf(std::span<IntLike> options) {
+  options_.reserve(options.size());
+  for (IntLike v : options) {
+    if (!std::in_range<int64_t>(v)) {
+      throw std::invalid_argument(
+          std::format("{} does not fit into int64_t in OneOf", v));
+    }
+    options_.push_back(static_cast<int64_t>(v));
+  }
+}
+
+template <typename T>
+template <std::integral IntLike>
+  requires std::same_as<T, int64_t>
+OneOf<T>::OneOf(std::initializer_list<IntLike> options) {
+  options_.reserve(options.size());
+  for (IntLike v : options) {
+    if (!std::in_range<int64_t>(v)) {
+      throw std::invalid_argument(
+          std::format("{} does not fit into int64_t in OneOf", v));
+    }
+    options_.push_back(static_cast<int64_t>(v));
+  }
+}
+
+template <typename T>
+template <std::convertible_to<std::string_view> StringLike>
+  requires std::same_as<T, std::string>
+OneOf<T>::OneOf(std::vector<StringLike> options) {
+  options_.reserve(options.size());
+  for (const auto& v : options) {
+    options_.push_back(static_cast<std::string>(v));
+  }
+}
+
+template <typename T>
+template <std::convertible_to<std::string_view> StringLike>
+  requires std::same_as<T, std::string>
+OneOf<T>::OneOf(std::span<StringLike> options) {
+  options_.reserve(options.size());
+  for (const auto& v : options) {
+    options_.push_back(static_cast<std::string>(v));
+  }
+}
+
+template <typename T>
+template <std::convertible_to<std::string_view> StringLike>
+  requires std::same_as<T, std::string>
+OneOf<T>::OneOf(std::initializer_list<StringLike> options) {
+  options_.reserve(options.size());
+  for (const auto& v : options) {
+    options_.push_back(static_cast<std::string>(v));
+  }
+}
+
+template <typename T>
+std::vector<T> OneOf<T>::GetOptions() const {
+  return options_;
+}
+
+template <typename T>
+std::string OneOf<T>::ToString(
+    std::function<std::string(const T&)> to_string) const {
+  std::string options = "{";
+  for (const auto& option : options_) {
+    if (options.size() > 1) options += ", ";
+    options += std::format("`{}`", to_string(option));
+  }
+  options += "}";
+
+  return std::format("is one of {}", options);
+}
+
+template <typename T>
+bool OneOf<T>::IsSatisfiedWith(const T& value) const {
+  return std::find(options_.begin(), options_.end(), value) != options_.end();
+}
+
+template <typename T>
+std::string OneOf<T>::Explanation(
+    std::function<std::string(const T&)> to_string, const T& value) const {
+  std::string options = "{";
+  for (const auto& option : options_) {
+    if (options.size() > 1) options += ", ";
+    options += std::format("`{}`", to_string(option));
+  }
+  options += "}";
+
+  return std::format("`{}` is not one of {}", to_string(value), options);
 }
 
 }  // namespace moriarty
--- src/variables/constraints/base_constraints_test.cc
+++ src/variables/constraints/base_constraints_test.cc
@@ -17,15 +17,21 @@
 #include "src/variables/constraints/base_constraints.h"
 
 #include <cstdint>
+#include <limits>
 #include <set>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 #include <type_traits>
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 namespace moriarty {
 namespace {
+
+using testing::ElementsAre;
+using testing::Throws;
 
 TEST(BaseConstraintsTest, ExactlyForVariousIntegerTypesWorks) {
   static_assert(std::is_same_v<decltype(Exactly(123)), Exactly<int64_t>>);
@@ -60,9 +66,221 @@ TEST(BaseConstraintsTest, ExactlyForVariousStringLikeTypesWorks) {
   EXPECT_EQ(Exactly(std::string_view("abc")).GetValue(), std::string("abc"));
 }
 
+TEST(BaseConstraintsTest, ExactlyForVariousArrayLikeTypesWorks) {
+  // TODO: The internals of vector are not fully being deduced the way we want
+  // them to (e.g., std::vector<int> instead of std::vector<int64_t>). We should
+  // fix that.
+
+  {  // initializer_list<int>
+    static_assert(std::is_same_v<decltype(Exactly({1, 2, 3})),
+                                 Exactly<std::vector<int>>>);
+  }
+  {  // span<const int>
+    std::vector<int> v = {1, 2, 3};
+    static_assert(std::is_same_v<decltype(Exactly(std::span<const int>{v})),
+                                 Exactly<std::vector<int>>>);
+  }
+  {
+    // span<int>
+    std::vector<int> v = {1, 2, 3};
+    static_assert(std::is_same_v<decltype(Exactly(std::span<int>{v})),
+                                 Exactly<std::vector<int>>>);
+  }
+  {  // initializer_list<std::string>
+    static_assert(std::is_same_v<decltype(Exactly(
+                                     {std::string("abc"), std::string("def")})),
+                                 Exactly<std::vector<std::string>>>);
+  }
+  {  // vector<std::string>
+    std::vector<std::string> v = {"abc", "def"};
+    static_assert(std::is_same_v<decltype(Exactly(v)),
+                                 Exactly<std::vector<std::string>>>);
+  }
+  {  // span<const std::string>
+    std::vector<std::string> v = {"abc", "def"};
+    static_assert(
+        std::is_same_v<decltype(Exactly(std::span<const std::string>{v})),
+                       Exactly<std::vector<std::string>>>);
+  }
+  {  // span<std::string>
+    std::vector<std::string> v = {"abc", "def"};
+    static_assert(std::is_same_v<decltype(Exactly(std::span<std::string>{v})),
+                                 Exactly<std::vector<std::string>>>);
+  }
+}
+
 TEST(BaseConstraintsTest, ExactlyForOtherTypesWorks) {
   EXPECT_EQ(Exactly(std::set<int>({1, 2, 3})).GetValue(),
             std::set<int>({1, 2, 3}));
+}
+
+TEST(BaseConstraintsTest, ExactlyToStringWorks) {
+  EXPECT_EQ(
+      Exactly(123).ToString([](const auto& v) { return std::to_string(v); }),
+      "is exactly 123");
+  EXPECT_EQ(Exactly("abc").ToString([](const auto& v) { return v; }),
+            "is exactly abc");
+}
+
+TEST(BaseConstraintsTest, ExactlyWithTooLargeOfIntegersShouldThrow) {
+  EXPECT_THAT([=]() { (void)Exactly(std::numeric_limits<uint64_t>::max()); },
+              Throws<std::invalid_argument>());
+}
+
+TEST(BaseConstraintsTest, ExactlyIsSatisfiedWithShouldWork) {
+  {
+    EXPECT_TRUE(Exactly(123).IsSatisfiedWith(123));
+    EXPECT_TRUE(Exactly("abc").IsSatisfiedWith("abc"));
+  }
+  {
+    EXPECT_FALSE(Exactly(123).IsSatisfiedWith(124));
+    EXPECT_FALSE(Exactly("abc").IsSatisfiedWith("def"));
+  }
+}
+
+TEST(BaseConstraintsTest, ExactlyExplanationShouldWork) {
+  auto int_to_string = [](const auto& v) { return std::to_string(v); };
+  auto str_to_string = [](const auto& v) { return v; };
+  EXPECT_EQ(Exactly(123).Explanation(int_to_string, 11),
+            "`11` is not exactly `123`");
+  EXPECT_EQ(Exactly("abc").Explanation(str_to_string, "hello"),
+            "`hello` is not exactly `abc`");
+}
+
+// ---------------------------------------------------------------------------
+//  OneOf
+
+TEST(BaseConstraintsTest, OneOfForVariousIntegerTypesWorks) {
+  {  // initializer_list
+    static_assert(std::is_same_v<decltype(OneOf({1, 2, 3})), OneOf<int64_t>>);
+    static_assert(std::is_same_v<decltype(OneOf({static_cast<int32_t>(1),
+                                                 static_cast<int32_t>(2),
+                                                 static_cast<int32_t>(3)})),
+                                 OneOf<int64_t>>);
+    static_assert(std::is_same_v<decltype(OneOf({static_cast<int64_t>(1),
+                                                 static_cast<int64_t>(2),
+                                                 static_cast<int64_t>(3)})),
+                                 OneOf<int64_t>>);
+    static_assert(std::is_same_v<decltype(OneOf({static_cast<uint32_t>(1),
+                                                 static_cast<uint32_t>(2),
+                                                 static_cast<uint32_t>(3)})),
+                                 OneOf<int64_t>>);
+    static_assert(std::is_same_v<decltype(OneOf({static_cast<uint64_t>(1),
+                                                 static_cast<uint64_t>(2),
+                                                 static_cast<uint64_t>(3)})),
+                                 OneOf<int64_t>>);
+  }
+  {  // vector
+    std::vector<int> v = {1, 2, 3};
+    static_assert(std::is_same_v<decltype(OneOf(v)), OneOf<int64_t>>);
+  }
+  {  // span
+    std::vector<int> v = {1, 2, 3};
+    static_assert(
+        std::is_same_v<decltype(OneOf(std::span<int>{v})), OneOf<int64_t>>);
+    static_assert(std::is_same_v<decltype(OneOf(std::span<const int>{v})),
+                                 OneOf<int64_t>>);
+  }
+
+  EXPECT_EQ(OneOf({1, 2, 3}).GetOptions(), std::vector<int64_t>({1, 2, 3}));
+  EXPECT_EQ(OneOf({static_cast<int32_t>(1), static_cast<int32_t>(2),
+                   static_cast<int32_t>(3)})
+                .GetOptions(),
+            std::vector<int64_t>({1, 2, 3}));
+  EXPECT_EQ(OneOf({static_cast<int64_t>(1), static_cast<int64_t>(2),
+                   static_cast<int64_t>(3)})
+                .GetOptions(),
+            std::vector<int64_t>({1, 2, 3}));
+  EXPECT_EQ(OneOf({static_cast<uint32_t>(1), static_cast<uint32_t>(2),
+                   static_cast<uint32_t>(3)})
+                .GetOptions(),
+            std::vector<int64_t>({1, 2, 3}));
+  EXPECT_EQ(OneOf({static_cast<uint64_t>(1), static_cast<uint64_t>(2),
+                   static_cast<uint64_t>(3)})
+                .GetOptions(),
+            std::vector<int64_t>({1, 2, 3}));
+}
+
+TEST(BaseConstraintsTest, OneOfForVariousStringTypesWorks) {
+  {  // initializer_list
+    static_assert(
+        std::is_same_v<decltype(OneOf({"abc", "def"})), OneOf<std::string>>);
+    static_assert(std::is_same_v<decltype(OneOf(
+                                     {std::string("abc"), std::string("def")})),
+                                 OneOf<std::string>>);
+    static_assert(std::is_same_v<decltype(OneOf({std::string_view("abc"),
+                                                 std::string_view("def")})),
+                                 OneOf<std::string>>);
+  }
+  {  // vector
+    std::vector<std::string> v = {"abc", "def"};
+    static_assert(std::is_same_v<decltype(OneOf(v)), OneOf<std::string>>);
+  }
+  {  // span
+    std::vector<std::string> v = {"abc", "def"};
+    static_assert(std::is_same_v<decltype(OneOf(std::span<std::string>{v})),
+                                 OneOf<std::string>>);
+    static_assert(
+        std::is_same_v<decltype(OneOf(std::span<const std::string>{v})),
+                       OneOf<std::string>>);
+  }
+
+  EXPECT_EQ(OneOf({"abc", "def"}).GetOptions(),
+            std::vector<std::string>({"abc", "def"}));
+  EXPECT_EQ(OneOf({std::string("abc"), std::string("def")}).GetOptions(),
+            std::vector<std::string>({"abc", "def"}));
+  EXPECT_EQ(
+      OneOf({std::string_view("abc"), std::string_view("def")}).GetOptions(),
+      std::vector<std::string>({"abc", "def"}));
+}
+
+TEST(BaseConstraintsTest, OneOfForOtherTypesWorks) {
+  EXPECT_THAT(
+      OneOf({std::set<int>({1, 2, 3}), std::set<int>({4, 5, 6})}).GetOptions(),
+      ElementsAre(std::set<int>({1, 2, 3}), std::set<int>({4, 5, 6})));
+}
+
+TEST(BaseConstraintsTest, OneOfToStringWorks) {
+  auto int_to_string = [](const auto& v) { return std::to_string(v); };
+  auto str_to_string = [](const auto& v) { return v; };
+
+  EXPECT_EQ(OneOf({123, 456}).ToString(int_to_string),
+            "is one of {`123`, `456`}");
+  EXPECT_EQ(OneOf({"abc", "def"}).ToString(str_to_string),
+            "is one of {`abc`, `def`}");
+}
+
+TEST(BaseConstraintsTest, OneOfWithTooLargeOfIntegersShouldThrow) {
+  // One is okay, one isn't. (ensuring we test multiple)
+  EXPECT_THAT(
+      [=]() {
+        (void)OneOf({uint64_t{1}, std::numeric_limits<uint64_t>::max()});
+      },
+      Throws<std::invalid_argument>());
+}
+
+TEST(BaseConstraintsTest, OneOfIsSatisfiedWithShouldWork) {
+  {
+    EXPECT_TRUE(OneOf({123, 456}).IsSatisfiedWith(123));
+    EXPECT_TRUE(OneOf({456}).IsSatisfiedWith(456));
+    EXPECT_TRUE(OneOf({"abc", "def", "hello"}).IsSatisfiedWith("abc"));
+    EXPECT_TRUE(OneOf({"abc", "def", "hello"}).IsSatisfiedWith("hello"));
+  }
+  {
+    EXPECT_FALSE(OneOf({123, 456}).IsSatisfiedWith(123567));
+    EXPECT_FALSE(OneOf({456}).IsSatisfiedWith(123));
+    EXPECT_FALSE(OneOf({"abc", "def", "hello"}).IsSatisfiedWith("ABC"));
+    EXPECT_FALSE(OneOf({"abc", "def", "hello"}).IsSatisfiedWith("ertert"));
+  }
+}
+
+TEST(BaseConstraintsTest, OneOfExplanationShouldWork) {
+  auto int_to_string = [](const auto& v) { return std::to_string(v); };
+  auto str_to_string = [](const auto& v) { return v; };
+  EXPECT_EQ(OneOf({123, 456}).Explanation(int_to_string, 11),
+            "`11` is not one of {`123`, `456`}");
+  EXPECT_EQ(OneOf({"abc", "def"}).Explanation(str_to_string, "hello"),
+            "`hello` is not one of {`abc`, `def`}");
 }
 
 }  // namespace
