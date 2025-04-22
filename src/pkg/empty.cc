ee8f0a36d47dcf48ac48428361e28fb8c684634a
Darcy Best <darcy.best.89@gmail.com>
2025-04-22 11:40:15 -0600

    Create a static export.

---
+++ src/pkg/empty.cc
@@ -0,0 +1,9 @@
+// This file is an empty husk used as a Bazel workaround.
+
+namespace moriarty::moriarty_internal {
+
+void DoAbsolutelyNothingThisIsAn_EmptyFile() {
+  // This file is intentionally left empty.
+}
+
+}  // namespace moriarty::moriarty_internal
