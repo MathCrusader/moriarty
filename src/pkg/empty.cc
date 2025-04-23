4a2699d2dd90066d667d483bb683b7930ac5668c
Darcy Best <darcy.best.89@gmail.com>
2025-04-23 11:33:52 -0600

    Use cc_static_library
    There is a new experimental feature in Bazel which does what we were attempting to automate. Using that for now.
    
    bazel build --experimental_cc_static_library //src/pkg/...

--- src/pkg/empty.cc
+++
@@ -1,9 +0,0 @@
-// This file is an empty husk used as a Bazel workaround.
-
-namespace moriarty::moriarty_internal {
-
-void DoAbsolutelyNothingThisIsAn_EmptyFile() {
-  // This file is intentionally left empty.
-}
-
-}  // namespace moriarty::moriarty_internal
